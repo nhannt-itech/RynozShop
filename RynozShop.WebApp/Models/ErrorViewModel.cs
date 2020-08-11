87436d014db0f577f1afd74182afa1cacaf8d974
nhannt-itech <67116951+nhannt-itech@users.noreply.github.com>
2020-08-11 19:49:25 +0700

    Add project files.

---
+++ RynozShop.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RynozShop.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
